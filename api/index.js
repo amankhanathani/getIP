4a74a14b76d546178a126e8ccd9485a1341fd780
Adithya Pai B <paiadithya26@gmail.com>
2021-09-14 20:37:32 +0530

    Browser update 🕸

--- api/index.js
+++ api/index.js
@@ -3,9 +3,9 @@ const app = require("express")().get("/", (req, res) => {
     IP:
       req.headers["x-forwarded-for"]?.split(",").shift() ||
       req.socket?.remoteAddress,
-    device : req.headers["user-agent"].split("(")[1].split(")")[0],
-    browser : req.headers["user-agent"].split(" ")[req.headers["user-agent"].split(" ").length - 1],
-    language: req.headers["accept-language"].split(",")[0],
+    device : req.headers["user-agent"].split("(")[1].split(")")[0]  ,
+    browser : req.headers["user-agent"].split("(") ,
+    language: req.headers["accept-language"]        
   });
 });
 module.exports = app;
