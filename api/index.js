69d487e9c7509e0188a40b1207930eff4ea3902a
Adithya Pai B <paiadithya26@gmail.com>
2021-09-14 20:18:37 +0530

    browser fixed

--- api/index.js
+++ api/index.js
@@ -3,10 +3,9 @@ const app = require("express")().get("/", (req, res) => {
     IP:
       req.headers["x-forwarded-for"]?.split(",").shift() ||
       req.socket?.remoteAddress,
-    OS: req.headers["user-agent"].split("(")[1].split(")")[0],
-    Browser: req.headers["user-agent"].split("(")[0],
-    Language: req.headers["accept-language"].split(",")[0],
-    Time: new Date().toLocaleString(),
+    device : req.headers["user-agent"].split("(")[1].split(")")[0],
+    browser: req.headers["user-agent"].split(" ")[1],
+    language: req.headers["accept-language"].split(",")[0],
   });
 });
 module.exports = app;
