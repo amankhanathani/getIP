34468ee473501860e95950c056a85f24c0cad8f0
Adithya Pai B <paiadithya26@gmail.com>
2021-09-14 12:45:12 +0530

    Added More options

--- api/index.js
+++ api/index.js
@@ -1,8 +1,12 @@
 const app = require("express")().get("/", (req, res) => {
-  res.json({
+  res.send({
     IP:
       req.headers["x-forwarded-for"]?.split(",").shift() ||
       req.socket?.remoteAddress,
+    OS: req.headers["user-agent"].split("(")[1].split(")")[0],
+    Browser: req.headers["user-agent"].split("(")[0],
+    Language: req.headers["accept-language"].split(",")[0],
+    Time: new Date().toLocaleString(),
   });
 });
 module.exports = app;
