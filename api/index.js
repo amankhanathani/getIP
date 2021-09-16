25e0f6b1af3ca48ed2556c1b99ba6506f0966302
Adithya Pai B <paiadithya26@gmail.com>
2021-09-16 11:10:17 +0530

    Added Front End

--- api/index.js
+++ api/index.js
@@ -1,11 +1,17 @@
-const app = require("express")().get("/", (req, res) => {
+const path = require("path");
+const app = require("express")();
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
+app.get("/*", (req, res) => {
   res.send({
     IP:
       req.headers["x-forwarded-for"]?.split(",").shift() ||
       req.socket?.remoteAddress,
-    device : req.headers["user-agent"].split("(")[1].split(")")[0]  ,
-    browser : req.headers["user-agent"].split("(") ,
-    language: req.headers["accept-language"]        
+    device: req.headers["user-agent"].split("(")[1].split(")")[0],
+    browser:
+      req.headers["user-agent"].split("(")[
+        req.headers["user-agent"].split("(").length - 1],
   });
 });
 module.exports = app;
